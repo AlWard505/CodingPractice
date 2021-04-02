b5d4d835da2cd9f6db965a0a56f37d1f64d48060
AlWard505 <60785111+AlWard505@users.noreply.github.com>
2021-04-02 22:25:32 +0100

    tested changing buttons due to window size

--- python/TkinterLayoutTest(2).py
+++ python/TkinterLayoutTest(2).py
@@ -1,17 +1,22 @@
 from tkinter import *
 x=0
 y=0
-z=32
+z=16
 main = PanedWindow()
+Grid.rowconfigure(main, 0, weight=1)
+Grid.columnconfigure(main, 0, weight=1)
 main.pack(fill=BOTH, expand=1,side = LEFT)
 
 frame1 = Frame(main,bd = 5, bg = "blue")
+frame1.grid(row=0, column=0, sticky=N+S+E+W)
 while x != z:
     x+=1
+    Grid.rowconfigure(frame1, x, weight=1)
     while y != z:
         y+=1
-        button = Button(frame1, image = "",height=1, width = 1)
-        button.grid(column = x, row = y)
+        Grid.columnconfigure(frame1, y, weight=1)
+        button = Button(frame1)
+        button.grid(column = x, row = y, sticky=N+S+E+W)  
 
     y = 0
 frame1.pack(anchor = NE)
