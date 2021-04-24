6933360b4c1edcb65612c68cd865fe1f24a7b86e
epicpigslav688 <epicpigslav@gmail.com>
2021-04-24 23:08:51 +0100

    multiply and divide

--- C#/Mathmatics/Mathmatics/Program.cs
+++ C#/Mathmatics/Mathmatics/Program.cs
@@ -7,18 +7,32 @@ namespace Mathmatics
         static void Main(string[] args)
         {
             Console.WriteLine("pick the first number to add");
-            int number1 = int.Parse(Console.ReadLine());
+            float number1 = float.Parse(Console.ReadLine());
             Console.WriteLine("pick the second number to add");
-            int number2 = int.Parse(Console.ReadLine());
-            int addednum = number1 + number2;
+            float number2 = float.Parse(Console.ReadLine());
+            float addednum = number1 + number2;
             Console.WriteLine(addednum);
 
             Console.WriteLine("pick the number to subtract from");
-            int number3 = int.Parse(Console.ReadLine());
+            float number3 = float.Parse(Console.ReadLine());
             Console.WriteLine("pick the number to subtract");
-            int number4 = int.Parse(Console.ReadLine());
-            int subtractednum = number3 - number4;
+            float number4 = float.Parse(Console.ReadLine());
+            float subtractednum = number3 - number4;
             Console.WriteLine(subtractednum);
+
+            Console.WriteLine("pick the number to multiply from");
+            float number5 = float.Parse(Console.ReadLine());
+            Console.WriteLine("pick the number to multiply by");
+            float number6 = float.Parse(Console.ReadLine());
+            float timesednum = number5 * number6;
+            Console.WriteLine(timesednum);
+
+            Console.WriteLine("pick the number to divide");
+            float number7 = float.Parse(Console.ReadLine());
+            Console.WriteLine("pick the number to divide by");
+            float number8 = float.Parse(Console.ReadLine());
+            float dividednum = number7 / number8;
+            Console.WriteLine(dividednum);
         }
     }
 }
