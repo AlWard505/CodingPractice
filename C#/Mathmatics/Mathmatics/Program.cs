d49215b1bde0e21043fb9c0432c57ad602360a80
epicpigslav688 <epicpigslav@gmail.com>
2021-04-24 22:57:55 +0100

    added varibles

---
+++ C#/Mathmatics/Mathmatics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mathmatics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pick the first number to add");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("pick the second number to add");
+            int number2 = int.Parse(Console.ReadLine());
+            int addednum = number1 + number2;
+            Console.WriteLine(addednum);
+        }
+    }
+}
