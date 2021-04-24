7309a0678bd660b8d51d071d4458e6ebd06209d7
epicpigslav688 <epicpigslav@gmail.com>
2021-04-24 22:47:06 +0100

    Set and printed a varible

---
+++ C#/02-Varibles/02-Varibles/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02_Varibles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string varible1 = "howdie planet";
+            Console.WriteLine(varible1);
+        }
+    }
+}
