443623c5c259399ddbf2d8260f4bad47fe8d4923
epicpigslav688 <epicpigslav@gmail.com>
2021-04-27 23:47:49 +0100

    if statement

---
+++ C#/04-Statements/04-Statements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word == "1"){
+                Console.WriteLine("it worked");
+            }
+        }
+    }
+}
