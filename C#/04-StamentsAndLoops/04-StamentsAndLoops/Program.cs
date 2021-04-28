96c2e9b785f0d18c75dfc883e174987e32046392
epicpigslav688 <epicpigslav@gmail.com>
2021-04-28 19:46:10 +0100

    added elif and else

--- C#/04-StamentsAndLoops/04-StamentsAndLoops/Program.cs
+++ C#/04-StamentsAndLoops/04-StamentsAndLoops/Program.cs
@@ -6,18 +6,29 @@ namespace _04_StamentsAndLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter 1 to print name");
+            Console.WriteLine("enter 1 to print name, 2 to print house and anything else to print car");
             string word = Console.ReadLine();
+
             if (word == "1")
             {
                 Console.WriteLine("name");
             }
 
+            else if (word == "2")
+            {
+                Console.WriteLine("house");
+            }
+
+            else
+            {
+                Console.WriteLine("car");
+            }
+            Console.WriteLine("Enter Key to continue");
             word = Console.ReadLine();
             while (word != "Key")
             {
-                word = Console.ReadLine();
                 Console.WriteLine("you did not enter Key");
+                word = Console.ReadLine();
             }
             Console.WriteLine("you entered the Key");
 
