1b48715d458babcc48e73b620bd5bdabd8d33cdd
epicpigslav688 <epicpigslav@gmail.com>
2021-04-28 23:16:41 +0100

    Arrays

---
+++ C#/06-Arrays/06-Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "stan", "greg", "steve" };
+            for(int i = 0;i < Names.Length; i++)
+            {
+                Console.WriteLine(Names[i]);
+            }
+
+            
+        }
+    }
+}
