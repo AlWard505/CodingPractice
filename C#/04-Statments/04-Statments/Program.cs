7cdcb41353b134cb37cbe66da3c47dd63b75f018
epicpigslav688 <epicpigslav@gmail.com>
2021-04-28 19:18:30 +0100

    fixed file issues and added while loop

---
+++ C#/04-Statments/04-Statments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_Statments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word == "1")
+            {
+                Console.WriteLine("it worked");
+            }
+            while (word != "Key")
+            {
+                word = Console.ReadLine();
+                Console.WriteLine("you did not enter Key");
+            }
+            Console.WriteLine("you entered the Key");
+        }
+    }
+}
