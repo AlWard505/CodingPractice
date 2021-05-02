9656d7ffe6a2fcadf86d75c3f30ddb3dcfef107f
epicpigslav688 <epicpigslav@gmail.com>
2021-05-03 00:00:27 +0100

    created a function with parameters

--- C#/08-Functions/08-Functions/Program.cs
+++ C#/08-Functions/08-Functions/Program.cs
@@ -8,10 +8,20 @@ namespace _08_Functions
         {
             Console.WriteLine("Hello World!");
             Function();
+            Console.WriteLine("enter a number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter a number to add to the other number");
+            int y = int.Parse(Console.ReadLine());
+            Function2(x, y);
+
         }
         static void Function()
         {
             Console.WriteLine("this is a function");
+        }
+        static void Function2(int x,int y)
+        {
+            Console.WriteLine(x + y);
         }
     }
 }
