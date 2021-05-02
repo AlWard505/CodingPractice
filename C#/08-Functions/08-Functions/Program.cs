bafec8dc0b1b528c55560879977cd0c068f72964
epicpigslav688 <epicpigslav@gmail.com>
2021-05-02 23:50:40 +0100

    Create Program.cs

---
+++ C#/08-Functions/08-Functions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Function();
+        }
+        static void Function()
+        {
+            Console.WriteLine("this is a function");
+        }
+    }
+}
