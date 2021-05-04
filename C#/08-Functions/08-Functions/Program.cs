de4e92d53cf08e35b27124ff1c5155677279ac38
epicpigslav688 <epicpigslav@gmail.com>
2021-05-04 15:13:58 +0100

    used a return

--- C#/08-Functions/08-Functions/Program.cs
+++ C#/08-Functions/08-Functions/Program.cs
@@ -13,6 +13,12 @@ namespace _08_Functions
             Console.WriteLine("enter a number to add to the other number");
             int y = int.Parse(Console.ReadLine());
             Function2(x, y);
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your age");
+            string age = Console.ReadLine();
+            string sentence = Function3(name, age);
+            Console.WriteLine(sentence);
 
         }
         static void Function()
@@ -22,6 +28,12 @@ namespace _08_Functions
         static void Function2(int x,int y)
         {
             Console.WriteLine(x + y);
+        }
+
+        static string Function3(string name, string age)
+        {
+            string sentence = ("my name is " + name + " and i am "+age);
+            return sentence;
         }
     }
 }
