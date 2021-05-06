407d20829694700260a87639dc8bb0cbf15cf863
epicpigslav688 <epicpigslav@gmail.com>
2021-05-06 22:21:28 +0100

    Used Break and continue in a loop

---
+++ C#/09-BreakAndContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09_BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 2)
+                {
+                    continue;
+                }
+
+                if (i == 8)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
