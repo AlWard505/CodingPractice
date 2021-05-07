5eab728ad9897c0ce80dac76613c47f27785cad9
epicpigslav688 <epicpigslav@gmail.com>
2021-05-07 21:45:36 +0100

    creates an account with an initial balance

---
+++ C#/10-Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new bankaccount(Console.ReadLine(), 1000);
+            Console.WriteLine($"Account {account.number} was created for {account.owner} with {account.balance} initial balance.");
+        }
+    }
+    public class bankaccount
+    {
+        public string number { get; }
+        public string owner { get; set; }
+        public decimal balance { get; }
+        public bankaccount(string name, decimal initialbalance)
+        {
+            this.owner = name;
+            this.balance = initialbalance;
+        }
+        public void deposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void withdrawl(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
