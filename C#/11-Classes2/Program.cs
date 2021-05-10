3e24f929965011754a23832587c32b499cc2cd37
epicpigslav688 <epicpigslav@gmail.com>
2021-05-10 19:38:37 +0100

    expermenting with classes (not working)

---
+++ C#/11-Classes2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _11_Classes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enemys = new Enemy();
+            Console.WriteLine(enemys);
+            enemys = new Enemy();
+            Console.WriteLine(enemys);
+        }
+    }
+    class Enemy
+    {
+        public int Health
+        {
+            get
+            {
+                int health = 0;
+                foreach(var list in Damage)
+                {
+                    health += list;
+                }
+                return health;
+            }
+        }
+        private List<int> Damage = new List<int>();
+
+        public Enemy()
+        {
+            Damage.Add(-10);
+        }
+
+        public void damage(int DamageTaken)
+        {
+            Damage.Add(DamageTaken);
+        }
+    }
+
+    
+}
