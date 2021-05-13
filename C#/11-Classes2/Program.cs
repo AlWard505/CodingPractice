b7fc07ae9a1c8866ee4745517974f17bae272061
epicpigslav688 <epicpigslav@gmail.com>
2021-05-13 21:38:51 +0100

    removed instance once health = 0

--- C#/11-Classes2/Program.cs
+++ C#/11-Classes2/Program.cs
@@ -15,17 +15,28 @@ namespace _11_Classes2
             }
             while (true)
             {
-                Console.WriteLine("ehich enemy do you want to damage");
+                Console.WriteLine("which enemy do you want to damage");
                 int x = 0;
                 foreach(var enemy in enemies)
                 {
                     
-                    Console.WriteLine("enemy"+ Convert.ToString(x));
+                    Console.WriteLine("enemy "+ Convert.ToString(x));
                     Console.WriteLine("health: " + enemy.Health);
                     x += 1;
                 }
                 int attacked = int.Parse(Console.ReadLine());
+                Console.WriteLine("how much damage");
                 enemies[attacked].damage(int.Parse(Console.ReadLine()));
+                if (enemies[attacked].Health <= 0)
+                {
+                    Console.WriteLine("the enemy is dead");
+                    enemies.RemoveAt(attacked);
+                }
+                if (enemies.Count == 0)
+                {
+                    Console.WriteLine("you win");
+                    break;
+                }
             }
         }
     }
@@ -53,10 +64,7 @@ namespace _11_Classes2
         public void damage(int DamageTaken)
         {
             Damage.Add(DamageTaken);
-            if (Health <= 0)
-            {
-                Console.WriteLine("the enemy is dead");
-            }
+            
         }
     }
 
