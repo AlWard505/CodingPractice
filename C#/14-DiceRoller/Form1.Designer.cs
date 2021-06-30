ea39012608d6e9c7e1cf032d4148cff9971234aa
epicpigslav688 <epicpigslav@gmail.com>
2021-06-30 23:45:34 +0100

    created a dice roller

---
+++ C#/14-DiceRoller/Form1.Designer.cs
@@ -0,0 +1,468 @@
+﻿
+namespace _14_DiceRoller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.D4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.D6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.D10 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.D8 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.D20 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.D12 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown9 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // D4
+            // 
+            this.D4.Location = new System.Drawing.Point(123, 37);
+            this.D4.Name = "D4";
+            this.D4.Size = new System.Drawing.Size(45, 29);
+            this.D4.TabIndex = 0;
+            this.D4.Text = "Roll";
+            this.D4.UseVisualStyleBackColor = true;
+            this.D4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "D4";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "D6";
+            // 
+            // D6
+            // 
+            this.D6.Location = new System.Drawing.Point(123, 72);
+            this.D6.Name = "D6";
+            this.D6.Size = new System.Drawing.Size(45, 29);
+            this.D6.TabIndex = 3;
+            this.D6.Text = "Roll";
+            this.D6.UseVisualStyleBackColor = true;
+            this.D6.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "D10";
+            // 
+            // D10
+            // 
+            this.D10.Location = new System.Drawing.Point(123, 140);
+            this.D10.Name = "D10";
+            this.D10.Size = new System.Drawing.Size(45, 29);
+            this.D10.TabIndex = 9;
+            this.D10.Text = "Roll";
+            this.D10.UseVisualStyleBackColor = true;
+            this.D10.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "D8";
+            // 
+            // D8
+            // 
+            this.D8.Location = new System.Drawing.Point(123, 105);
+            this.D8.Name = "D8";
+            this.D8.Size = new System.Drawing.Size(45, 29);
+            this.D8.TabIndex = 6;
+            this.D8.Text = "Roll";
+            this.D8.UseVisualStyleBackColor = true;
+            this.D8.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "D20";
+            // 
+            // D20
+            // 
+            this.D20.Location = new System.Drawing.Point(123, 210);
+            this.D20.Name = "D20";
+            this.D20.Size = new System.Drawing.Size(45, 29);
+            this.D20.TabIndex = 15;
+            this.D20.Text = "Roll";
+            this.D20.UseVisualStyleBackColor = true;
+            this.D20.Click += new System.EventHandler(this.D20_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "D12";
+            // 
+            // D12
+            // 
+            this.D12.Location = new System.Drawing.Point(123, 175);
+            this.D12.Name = "D12";
+            this.D12.Size = new System.Drawing.Size(45, 29);
+            this.D12.TabIndex = 12;
+            this.D12.Text = "Roll";
+            this.D12.UseVisualStyleBackColor = true;
+            this.D12.Click += new System.EventHandler(this.D12_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 20);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "D";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 29);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 249);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 20);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "D100";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 29);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Roll";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(226, 330);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(154, 20);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "You Haven\'t rolled yet";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(46, 39);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 27);
+            this.numericUpDown1.TabIndex = 26;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(46, 107);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(71, 27);
+            this.numericUpDown2.TabIndex = 27;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(54, 144);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(63, 27);
+            this.numericUpDown3.TabIndex = 28;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(54, 177);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(63, 27);
+            this.numericUpDown4.TabIndex = 29;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(54, 212);
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(63, 27);
+            this.numericUpDown5.TabIndex = 30;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(62, 245);
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(55, 27);
+            this.numericUpDown6.TabIndex = 31;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(33, 280);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(49, 27);
+            this.numericUpDown7.TabIndex = 32;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(88, 280);
+            this.numericUpDown8.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(49, 27);
+            this.numericUpDown8.TabIndex = 33;
+            this.numericUpDown8.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown9
+            // 
+            this.numericUpDown9.Location = new System.Drawing.Point(46, 72);
+            this.numericUpDown9.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown9.Name = "numericUpDown9";
+            this.numericUpDown9.Size = new System.Drawing.Size(71, 27);
+            this.numericUpDown9.TabIndex = 34;
+            this.numericUpDown9.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 520);
+            this.Controls.Add(this.numericUpDown9);
+            this.Controls.Add(this.numericUpDown8);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.D20);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.D12);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.D10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.D8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.D6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.D4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button D4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button D6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button D10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button D8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button D20;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button D12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+        private System.Windows.Forms.NumericUpDown numericUpDown9;
+    }
+}
+
---
+++ C#/14-DiceRoller/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _14_DiceRoller
+{
+
+
+    public partial class Form1 : Form
+    {
+        Random rollD4 = new Random();
+        Random rollD6 = new Random();
+        Random rollD8 = new Random();
+        Random rollD10 = new Random();
+        Random rollD12 = new Random();
+        Random rollD20 = new Random();
+        Random rollCustom = new Random();
+        int roll = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown1.Value); i++)
+            {
+                roll += rollD4.Next(1, 5);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown9.Value); i++)
+            {
+                roll += rollD6.Next(1, 7);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown2.Value); i++)
+            {
+                roll += rollD4.Next(1, 9);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown3.Value); i++)
+            {
+                roll += rollD4.Next(1, 11);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void D20_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown5.Value); i++)
+            {
+                roll += rollD4.Next(1, 21);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown7_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D12_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void D12_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown4.Value); i++)
+            {
+                roll += rollD4.Next(1, 13);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown6.Value); i++)
+            {
+                roll += rollD4.Next(1, 101);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown8.Value); i++)
+            {
+                roll += rollD4.Next(1, Convert.ToInt32(numericUpDown7.Value)+1);
+            }
+            label9.Text = ("You rolled a " + roll);
+            roll = 0;
+        }
+    }
+}
