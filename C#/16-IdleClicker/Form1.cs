f41be59d0c4775b75f6a9146a6471074fb7d7e4f
epicpigslav688 <epicpigslav@gmail.com>
2021-08-25 01:15:09 +0100

    clicker, click the button and the number goes up

---
+++ C#/16-IdleClicker/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16_IdleClicker
+{
+    public partial class Form1 : Form
+    {
+        int clicks = 0;
+        int CP = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CLICK_Click(object sender, EventArgs e)
+        {
+            clicks += CP;
+            label1.Text = "CLICKS\n"+clicks;
+        }
+    }
+}
