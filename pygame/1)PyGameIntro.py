d09987a945f980b065773a532d305cfa5d3dae5f
epicpigslav688 <epicpigslav@gmail.com>
2021-09-01 01:21:40 +0100

    set up colours and set the background to white

--- pygame/1)PyGameIntro.py
+++ pygame/1)PyGameIntro.py
@@ -1,12 +1,22 @@
 import pygame, sys
 from pygame.locals import *
 
+#colours
+colours = {
+    "black" : (0,0,0),
+    "white" : (255,255,255),
+    "red" : (255,0,0),
+    "green" : (0,255,0),
+    "blue" : (0,0,255)
+    }
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('PyGameIntro')
+DISPLAYSURF.fill(colours["white"])
 while True: # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update()
+    pygame.display.update()
+
