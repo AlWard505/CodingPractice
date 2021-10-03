ea07702c7f142774b99998e4f71850332ba03e8a
Alice Ward <epicpigslav@gmail.com>
2021-10-03 20:24:22 +0100

    sets a bitmap to black

---
+++ C#/17-WindowsForms/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width = 640;
+            int height = 480;
+
+            Bitmap bmp = new Bitmap(width, height);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    bmp.SetPixel(x, y, Color.FromArgb(255, 0,0,0));
+                    pictureBox1.Image = bmp;
+                }
+            }
+            
+        }
+    }
+}
